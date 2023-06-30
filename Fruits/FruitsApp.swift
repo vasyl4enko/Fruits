5c7150ea877d4b403a4f51489d2f47efec4e839d
Oleksandr <oleksandr@MacBook-Pro-Oleksandr.local>
2023-06-30 12:25:54 +0300

    init

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Oleksandr on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
